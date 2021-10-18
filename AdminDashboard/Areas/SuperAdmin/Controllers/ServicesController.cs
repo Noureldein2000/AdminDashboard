59eea39adbb544d23ecd3ab7994026e9e09db9a5
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2021-10-18 13:27:44 +0200

    Implement Service Module at header bar

---
+++ AdminDashboard/Areas/SuperAdmin/Controllers/ServicesController.cs
@@ -0,0 +1,230 @@
+﻿using AdminDashboard.Areas.SuperAdmin.Models;
+using AdminDashboard.Models;
+using AdminDashboard.Models.SwaggerModels;
+using AdminDashboard.SwaggerClient;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Areas.SuperAdmin.Controllers
+{
+    [Area("SuperAdmin")]
+    [Route("SuperAdmin/[controller]/{action}/{id?}")]
+    [Authorize]
+    public class ServicesController : Controller
+    {
+        private readonly IAdminServiceApi api;
+        private readonly IDenominationApi apiDenomination;
+        public ServicesController(
+            )
+        {
+            //string urlIdentity = "https://localhost:44303";
+            string urlTms = "https://localhost:44321";
+            api = new AdminServiceApi(urlTms);
+            apiDenomination = new DenominationApi(urlTms);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int page = 1, int size = 10)
+        {
+            var data = await api.ApiAdminServiceGetServicesGetAsync(page, size);
+
+            var viewModel = new PagedResult<AdminServiceViewModel>
+            {
+                Results = data.Results.Select(u => Map(u)).ToList(),
+                PageCount = (int)data.PageCount,
+                CurrentPage = page,
+                PageSize = size
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            var serviceEntities = api.ApiAdminServiceGetServiceEntitiesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            var servicesCategories = api.ApiAdminServiceGetServicesCategoriesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            var serviceTypes = api.ApiAdminServiceGetServiceTypesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            AdminServiceViewModel viewModel = new AdminServiceViewModel()
+            {
+                ServiceEntities = serviceEntities,
+                ServicesCategories = servicesCategories,
+                ServiceTypes = serviceTypes
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AdminServiceViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var serviceEntities = api.ApiAdminServiceGetServiceEntitiesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                var servicesCategories = api.ApiAdminServiceGetServicesCategoriesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                var serviceTypes = api.ApiAdminServiceGetServiceTypesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                model.ServiceEntities = serviceEntities;
+                model.ServicesCategories = servicesCategories;
+                model.ServiceTypes = serviceTypes;
+
+                return View(model);
+            }
+
+            api.ApiAdminServiceAddServicePost(new AddServiceModel(
+                name: model.Name,
+                arName: model.ArName,
+                status: false,
+                serviceTypeID: model.ServiceTypeID,
+                code: model.Code,
+                serviceEntityID: model.ServiceEntityID,
+                serviceCategoryID: model.ServiceCategoryID,
+                pathClass: model.PathClass,
+                classType: model.ClassType
+                ));
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var data = api.ApiAdminServiceGetServiceByIdIdGet(id);
+
+            var serviceEntities = api.ApiAdminServiceGetServiceEntitiesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            var servicesCategories = api.ApiAdminServiceGetServicesCategoriesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            var serviceTypes = api.ApiAdminServiceGetServiceTypesGet().Select(a => new SelectListItem
+            {
+                Text = a.Name,
+                Value = a.Id.ToString()
+            }).ToList();
+
+            var viewModel = Map(data);
+            viewModel.ServiceEntities = serviceEntities;
+            viewModel.ServicesCategories = servicesCategories;
+            viewModel.ServiceTypes = serviceTypes;
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(AdminServiceViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var serviceEntities = api.ApiAdminServiceGetServiceEntitiesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                var servicesCategories = api.ApiAdminServiceGetServicesCategoriesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                var serviceTypes = api.ApiAdminServiceGetServiceTypesGet().Select(a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                }).ToList();
+
+                model.ServiceEntities = serviceEntities;
+                model.ServicesCategories = servicesCategories;
+                model.ServiceTypes = serviceTypes;
+
+                return View(model);
+            }
+
+            api.ApiAdminServiceEditServicePut(new EditServiceModel(
+                id:model.Id,
+                name: model.Name,
+                arName: model.ArName,
+                status: model.Status,
+                serviceTypeID: model.ServiceTypeID,
+                code: model.Code,
+                serviceEntityID: model.ServiceEntityID,
+                serviceCategoryID: model.ServiceCategoryID,
+                pathClass: model.PathClass,
+                classType: model.ClassType
+                ));
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult ChangeStatus(int id)
+        {
+            api.ApiAdminServiceChangeStatusPut(id);
+            return RedirectToAction(nameof(Index));
+        }
+        private AdminServiceViewModel Map(AdminServiceModel model)
+        {
+            return new AdminServiceViewModel
+            {
+                Id = model.Id,
+                Name = model.Name,
+                ArName = model.ArName,
+                ServiceTypeID = model.ServiceTypeID,
+                ServiceTypeName = model.ServiceTypeName,
+                ServiceCategoryID = model.ServiceCategoryID,
+                ServiceCategoryName = model.ServiceCategoryName,
+                Status = (bool)model.Status,
+                Code = model.Code,
+                ServiceEntityID = model.ServiceEntityID,
+                ServiceEntityName = model.ServiceEntityName,
+                CreationDate = model.CreationDate,
+                ClassType = model.ClassType
+            };
+        }
+
+    }
+}
---
+++ AdminDashboard/Areas/SuperAdmin/Models/AdminServiceViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AdminDashboard.Models.SwaggerModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Areas.SuperAdmin.Models
+{
+    public class AdminServiceViewModel
+    {
+        public int? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ArName { get; set; }
+        public bool Status { get; set; }
+        [Required]
+        public int? ServiceTypeID { get; set; }
+        public string ServiceTypeName { get; set; }
+        public string Code { get; set; }
+        [Required]
+        public int? ServiceEntityID { get; set; }
+        public string ServiceEntityName { get; set; }
+        [Required]
+        public int? ServiceCategoryID { get; set; }
+        public string ServiceCategoryName { get; set; }
+        public string PathClass { get; set; }
+        [Required]
+        [EnumDataType(typeof(ServiceClassType), ErrorMessage = "You Should choose a Class Type")]
+        public ServiceClassType ClassType { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public List<SelectListItem> ServiceEntities { get; set; }
+        public List<SelectListItem> ServicesCategories { get; set; }
+        public List<SelectListItem> ServiceTypes { get; set; }
+    }
+}
