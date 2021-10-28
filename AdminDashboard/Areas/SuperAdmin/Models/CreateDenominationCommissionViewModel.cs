bf795b5b363ec7c365fd8c3327744a26c9005e5e
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2021-10-28 17:50:35 +0200

    Validation Required for Demination Fee and Commission

--- AdminDashboard/Areas/SuperAdmin/Models/CreateDenominationCommissionViewModel.cs
+++ AdminDashboard/Areas/SuperAdmin/Models/CreateDenominationCommissionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace AdminDashboard.Areas.SuperAdmin.Models
     public class CreateDenominationCommissionViewModel
     {
         public int Id { get; set; }
+        [Required]
         public int CommissionId { get; set; }
         public int DenominationId { get; set; }
         public List<SelectListItem> Commissions{ get; set; }
