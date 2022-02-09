32c24d2b617a7b53bc9e43c900094e7ae865f865
Noureldein Hussein <nour.hussein@momkn.com.eg>
2022-02-09 11:47:34 +0000

    [#NT-9] [#NT-10] - Change Denomination Fee and Commission Grid

--- AdminDashboard/Areas/SuperAdmin/Models/CreateDenominationCommissionViewModel.cs
+++ AdminDashboard/Areas/SuperAdmin/Models/CreateDenominationCommissionViewModel.cs
@@ -13,6 +13,7 @@ namespace AdminDashboard.Areas.SuperAdmin.Models
         [Required]
         public int CommissionId { get; set; }
         public int DenominationId { get; set; }
+        public string DenominationName { get; set; }
         public List<SelectListItem> Commissions{ get; set; }
     }
 }
